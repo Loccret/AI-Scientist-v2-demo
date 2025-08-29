1b4fd8a90972bde72b93bdd8c80fe58a185657f3
Linda <loccret@gmail.com>
2025-08-29 19:46:11 +0900

    change name

--- run_ai.bash
+++
@@ -1,8 +0,0 @@
-python launch_scientist_bfts.py \
-    --load_ideas "ai_scientist/my-idea/SO-NEAT.json" \
-    --load_code \
-    --model_writeup "deepseek-reasoner" \
-    --model_citation "deepseek-chat" \
-    --model_review "deepseek-chat" \
-    --model_agg_plots "deepseek-reasoner" \
-    --num_cite_rounds 20
